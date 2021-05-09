561d7954dbe745f781b2252825ff80e5da70a112
Hasib Rafi <hasibulislam405@gmail.com>
2021-05-09 17:03:28 +0600

    json, API conected

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,13 +9,40 @@ for (var i=0; i<updateBtns.length; i++)
 
         console.log('productId:', productId, 'action:', action, 'User:', user)
 
-        if(user === 'AnonymousUser')
+        if(user === 'AnonymousUser') 
         {
             console.log("User is not logged in!")
         }
         else
         {
-            console.log("User is logged in!")
+            updateUserOrder(productId, action)
         }
     })
+}
+
+function updateUserOrder(productId, action)
+{
+    console.log("User is logged in, sending data..")
+
+    var url = '/update_item/'
+
+    fetch(url,  
+        {method:'POST', 
+        headers: {
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        }, 
+        
+        body:JSON.stringify ({'productId': productId, 'action': action})
+    })
+
+    .then((response)=>{
+        return response.json()
+    })
+
+    .then((data)=>{
+        console.log('data:', data)
+    })
+
+
 }
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from . models import *
 
 # Create your views here.
@@ -35,3 +36,7 @@ def checkout(request):
 
     context = {'items':items, 'order':order}
     return render (request,'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse("Item added!", safe=False)
