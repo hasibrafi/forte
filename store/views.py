6b7c249fa4621116e7b2197f3e8c49f659b05006
Hasib Rafi <hasibulislam405@gmail.com>
2021-05-30 18:56:56 +0600

    guest functionalities added

--- store/views.py
+++ store/views.py
@@ -32,9 +32,23 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('Cart: ', cart)
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart:
+            cartItems += cart[i]['quantity']
+
+            product = Product.objects.get(id=i)
+            total =  (product.price * cart[i]['quantity'])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]['quantity']
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render (request,'store/cart.html', context)
