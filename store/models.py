0df9ffbbfeadb63008d004d1d5aeb5319dab6e76
Hasib Rafi <hasibulislam405@gmail.com>
2021-06-09 02:53:11 +0600

    PayPal integration added

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    digital = models.BooleanField(default=False, null=True, blank=False)
+    digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True )
 
     def __str__(self):
@@ -33,11 +33,11 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False, null=True, blank=False)
+    complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        return str (self.id)
+        return str(self.id)
 
     @property
     def shipping(self):
@@ -64,8 +64,8 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0,null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
--- store/views.py
+++ store/views.py
@@ -12,80 +12,28 @@ def index(request):
     return render (request,'store/index.html', context)
 
 def store(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+    cartItems = data['cartItems'] 
 
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
     return render (request,'store/store.html', context)
 
 def cart(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-        except:
-            cart = {}
-        print('Cart: ', cart)
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
-
-        for i in cart:
-            try:
-                cartItems += cart[i]['quantity']
-
-                product = Product.objects.get(id=i)
-                total =  (product.price * cart[i]['quantity'])
-
-                order['get_cart_total'] += total
-                order['get_cart_items'] += cart[i]['quantity']
-
-                item = {
-                    'product':{
-                        'id': product.id, 
-                        'name': product.name,
-                        'price': product.price,
-                        'imageURL': product.imageURL,
-                    },
-                    'quantity': cart[i]['quantity'],
-                    'get_total': total
-                }
-
-                items.append(item)
-
-                if product.digital == False:
-                    order['shipping'] = True
-                
-            except:
-                pass
-
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render (request,'store/cart.html', context)
 
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render (request,'store/checkout.html', context)
@@ -125,25 +73,24 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-        
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer= customer,
-                order= order,
-                address= data['shipping']['address'],
-                city= data['shipping']['city'],
-                zipcode= data['shipping']['zipcode'],
-               
-            )
-
 
     else:
-        print('User is not logged in...')
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+        
+    if total == float(order.get_cart_total):
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer= customer,
+            order= order,
+            address= data['shipping']['address'],
+            city= data['shipping']['city'],
+            zipcode= data['shipping']['zipcode'],
+        )
 
     return JsonResponse("Payment successful!", safe=False)
